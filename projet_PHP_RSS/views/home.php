4cf7920397497ca30c9a4700476bd74ca0a6e348
STS076 <sophfrom76@hotmail.com>
2022-07-04 10:02:42 +0200

    header_footer_top

---
+++ projet_PHP_RSS/views/home.php
@@ -0,0 +1,11 @@
+<?php include '../elements/top.php' ?>
+
+<body>
+    <?php include '../elements/header.php' ?>
+
+    <h1 class="text-center">Hello World</h1>
+
+    <?php include '../elements/footer.php' ?>
+</body>
+
+</html>
---
+++ projet_PHP_RSS/views/pages.php
@@ -0,0 +1,8 @@
+<?php include '../elements/top.php' ?>
+
+<body>
+    <?php include '../elements/header.php' ?>
+    <?php include '../elements/footer.php' ?>
+</body>
+
+</html>
